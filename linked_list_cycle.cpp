afba32a89c912047bb76fc607b603b5d9e6ae026
shengyang wang <shengyangwang@shengyangs-MacBook-Pro.local>
2016-01-27 18:02:30 -0800

    some change

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode *slow=head, *fast = head->next;
+        while(fast&&fast->next){
+            if(slow->val==fast->val) return true;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return false;
+    }
+};
