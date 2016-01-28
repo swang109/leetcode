248c452e18f6c72a321d78b927651ba2a8bd7e43
shengyang wang <shengyangwang@shengyangs-MacBook-Pro.local>
2016-01-28 13:18:28 -0800

    one hard

--- first_missing_positive.cpp
+++ first_missing_positive.cpp
@@ -11,7 +11,7 @@ At last, the first place where its number is not right, return the place + 1.
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n;i++){
-            while(nums[i]>0&&nums[i]<=n&&nums[nums[i]-1]!=nums[i]){
+            while(nums[i]>0&&nums[i]<=n&&nums[nums[i]-1]!=nums[i]){  //WHILE!!!!
                 swap(nums[i], nums[nums[i]-1]);
             }
         }
